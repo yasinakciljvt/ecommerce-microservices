d948c060b48764a980ce60247d3b1ae263e43f25
= <=>
2026-01-28 20:11:19 +0300

    Add Order Service

---
+++ order/order/src/main/java/com/ecommerce/order/Repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.order.Repository;
+
+
+import com.ecommerce.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ order/order/src/main/java/com/ecommerce/order/dto/OrderResponse.java
@@ -0,0 +1,20 @@
+package com.ecommerce.order.dto;
+
+
+import com.ecommerce.order.model.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class OrderResponse {
+    private Long id;
+    private BigDecimal totalAmount;
+    private OrderStatus status;
+    private List<OrderItemDTO> items;
+    private LocalDateTime createdAt;
+}
