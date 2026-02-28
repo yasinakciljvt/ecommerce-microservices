2a42f3adcc7a22c353fab5e25fc6bf90d960886d
= <=>
2026-02-28 23:46:20 +0300

    add notification

---
+++ norification/src/main/java/com/ecommerce/norification/NorificationApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.norification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NorificationApplication.class, args);
+    }
+
+}
---
+++ norification/src/test/java/com/ecommerce/norification/NorificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.norification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
